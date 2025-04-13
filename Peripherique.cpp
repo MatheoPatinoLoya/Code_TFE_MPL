3abe6eef273837f243604d9e0de9c6ccaf641be1
MatheoPatinoLoya <matheopatinoloya@gmail.com>
2025-04-13 15:17:01 +0200

    Mise en ordre
    fonction ajouter + retirer externe
    Prochain comit afficher le volume et la sation sur l'ecran spi

--- Peripherique.cpp
+++ Peripherique.cpp
@@ -1,82 +1,80 @@
-  #include "Peripherique.h"
+#include "Peripherique.h"
 
-  RDA5807M radio;
-  int dernierEtatCLK = 0;
-  int dernierEtatCLK1 = 0;
-  int stationActuelle = STATION_INITIALE;
+RDA5807M radio;
+int dernierEtatCLK = 0;
+int dernierEtatCLK1 = 0;
+int stationActuelle = STATION_INITIALE;
 
-  int volume = 15;
-  bool modeMono = true;
-  bool modeMute = false;
+int volume = 15;
 
-  void Volume(void) {
-    int etatComparateur = digitalRead(CLK);
+void Volume(void) {
+  int etatComparateur = digitalRead(CLK);
 
-    if (etatComparateur != dernierEtatCLK) {
-    
-      if (digitalRead(DT) != etatComparateur) {
-        if (volume < MAX_VOLUME) volume++;
-      } else {
-        if (volume > 0) volume--;
-      }
+  if (etatComparateur != dernierEtatCLK) {
 
-      radio.setVolume(volume);
-      afficherVolume(volume);
+    if (digitalRead(DT) != etatComparateur) {
+      if (volume < MAX_VOLUME) volume++;
+    } else {
+      if (volume > 0) volume--;
     }
 
-    dernierEtatCLK = etatComparateur;
-  }
-  void Station(void) {
-    int etatComparateur1 = digitalRead(CLK1);
-    if (etatComparateur1 != dernierEtatCLK1) {
-      if (digitalRead(DT1) != etatComparateur1) {
-        stationActuelle = (stationActuelle < MAX_STATION) ? stationActuelle + 10 : MIN_STATION;
-      } else {
-        stationActuelle = (stationActuelle > MIN_STATION) ? stationActuelle - 10 : MAX_STATION;
-      }
-      radio.setBandFrequency(FIX_BAND, stationActuelle);
-      afficherStation();
-    }
-    dernierEtatCLK1 = etatComparateur1;
-  }
-  void Audio(void) {
-    int16_t audioSampleLeft = (analogRead(ADC_PIN_LEFT) - 512) * 64;
-    int16_t audioSampleRight = (analogRead(ADC_PIN_RIGHT) - 512) * 64;
-    I2S.write(audioSampleLeft);
-    I2S.write(audioSampleRight);
-    delayMicroseconds(22);
-  }
-  ////////////////////////////////////////////////////////////////
-  void afficherVolume(int vol) {
-    Serial.print("Volume actuel : ");
-    Serial.println(vol);
-  }
-  int afficherStation() {
-    Serial.print("Nouvelle station : ");
-    Serial.println(stationActuelle / 100.0, 1);
-    return stationActuelle;
-  }
-  bool initi_radio(bool mono, bool mute) {
-    if (!radio.initWire(Wire)) {
-      return false;
-    }
-    radio.setup(RADIO_FMSPACING, RADIO_FMSPACING_100);   // Espacement pour l'Europe
-    radio.setup(RADIO_DEEMPHASIS, RADIO_DEEMPHASIS_50);  // Déaccentuation pour l'Europe
-    radio.setBandFrequency(FIX_BAND, STATION_INITIALE);  // Fixer la station
     radio.setVolume(volume);
-    radio.setMono(mono);
-    radio.setMute(mute);
-    return true;
-  }
-  void Pin_Mode(void) {
-    pinMode(CLK, INPUT);
-    pinMode(DT, INPUT);
-    pinMode(CLK1, INPUT);
-    pinMode(DT1, INPUT);
-  }
-  void Lire_CLK(void) {
-    dernierEtatCLK = digitalRead(CLK);
-    dernierEtatCLK1 = digitalRead(CLK1);
+    afficherVolume(volume);
   }
 
-  ///////////////////////////////////////////////////////////////
+  dernierEtatCLK = etatComparateur;
+}
+void Station(void) {
+  int etatComparateur1 = digitalRead(CLK1);
+  if (etatComparateur1 != dernierEtatCLK1) {
+    if (digitalRead(DT1) != etatComparateur1) {
+      stationActuelle = (stationActuelle < MAX_STATION) ? stationActuelle + 10 : MIN_STATION;
+    } else {
+      stationActuelle = (stationActuelle > MIN_STATION) ? stationActuelle - 10 : MAX_STATION;
+    }
+    radio.setBandFrequency(FIX_BAND, stationActuelle);
+    afficherStation();
+  }
+  dernierEtatCLK1 = etatComparateur1;
+}
+void Audio(void) {
+  int16_t audioSampleLeft = (analogRead(ADC_PIN_LEFT) - 300) * 64;    // son de qualitée 300 pas 512
+  int16_t audioSampleRight = (analogRead(ADC_PIN_RIGHT) - 300) * 64;  // son de qualitée 300 pas 512
+  I2S.write(audioSampleLeft);
+  I2S.write(audioSampleRight);
+  delayMicroseconds(22);
+}
+////////////////////////////////////////////////////////////////
+void afficherVolume(int vol) {
+  Serial.print("Volume actuel : ");
+  Serial.println(vol);
+}
+int afficherStation(void) {
+  Serial.print("Nouvelle station : ");
+  Serial.println(stationActuelle / 100.0, 1);
+  return stationActuelle;
+}
+void Initi_Radio(void) {
+  radio.initWire(Wire);
+  radio.setup(RADIO_FMSPACING, RADIO_FMSPACING_100);   // Espacement pour l'Europe
+  radio.setup(RADIO_DEEMPHASIS, RADIO_DEEMPHASIS_50);  // Déaccentuation pour l'Europe
+  radio.setBandFrequency(FIX_BAND, STATION_INITIALE);  // Fixer la station
+  radio.setVolume(volume);
+  radio.setMono(true);
+  radio.setMute(true);
+}
+void Pin_Mode(void) {
+  pinMode(CLK, INPUT);
+  pinMode(DT, INPUT);
+  pinMode(CLK1, INPUT);
+  pinMode(DT1, INPUT);
+}
+void Lire_CLK(void) {
+  dernierEtatCLK = digitalRead(CLK);
+  dernierEtatCLK1 = digitalRead(CLK1);
+}
+
+///////////////////////////////////////////////////////////////
+void Initi_I2S(void) {
+  I2S.begin(I2S_PHILIPS_MODE, 44100, 16);  // Initialisation de l'I²S mode Philips, 44.1 kHz, 16 bits44100
+}
--- Peripherique.h
+++ Peripherique.h
@@ -6,42 +6,30 @@
 #include <RDA5807M.h>
 #include <I2S.h>
 
-#define CLK 13  
+#define CLK 13
 #define DT 11
-#define CLK1 A3  
+#define CLK1 A3
 #define DT1 A4
-#define ADC_PIN_LEFT A0   
-#define ADC_PIN_RIGHT A1  
-#define I2S_BCLK_PIN 1    
-#define I2S_LRC_PIN 0     
-#define I2S_DIN_PIN 9     
+#define ADC_PIN_LEFT A0
+#define ADC_PIN_RIGHT A1
+#define I2S_BCLK_PIN 1
+#define I2S_LRC_PIN 0
+#define I2S_DIN_PIN 9
 
-#define FIX_BAND RADIO_BAND_FM  
-#define MAX_VOLUME 25           
-#define MIN_STATION 8930        
-#define MAX_STATION 10500       
-#define STATION_INITIALE 9300   
+#define FIX_BAND RADIO_BAND_FM
+#define MAX_VOLUME 25
+#define MIN_STATION 8930
+#define MAX_STATION 10500
+#define STATION_INITIALE 9300
 
-// #define TFT_CS 10
-// #define TFT_RST 6
-// #define TFT_DC 12
-
-// #define PWM A2
-
-
-extern RDA5807M radio;
-extern int dernierEtatCLK, dernierEtatCLK1;
-extern int stationActuelle;
-extern int volume;
-
-
-void Volume(void);//chatgpt et reflexion de ma part 
-void Station(void);//chatgpt et reflexion de ma part 
-void Audio(void);//chatgpt et reflexion de ma part 
+void Volume(void);   //chatgpt et reflexion de ma part
+void Station(void);  //chatgpt et reflexion de ma part
+void Audio(void);    //chatgpt et reflexion de ma part
 ////////////////////////////////
-void afficherVolume(int vol);  //Fait part moi 
-int afficherStation(void);     // Fait part moi 
-bool initi_radio(bool mono,bool mute ) ;// chatgpt et reflexion de ma part 
-void Pin_Mode(void);// Fait part moi 
-void Lire_CLK(void);//Fait part moi
+void afficherVolume(int vol);  //Fait part moi
+int afficherStation(void);     // Fait part moi
+void Initi_Radio(void);            // chatgpt et reflexion de ma part
+void Pin_Mode(void);           // Fait part moi
+void Lire_CLK(void);           //Fait part moi
 ////////////////////////////////
+void Initi_I2S(void);
